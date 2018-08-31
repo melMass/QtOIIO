cafae0ebf5acb55c01d4776e8ff622d4b80be3e8
melmass <melmassadian@gmail.com>
2018-08-31 16:24:48 +0200

    added make to the build script

--- build_osx_brew.sh
+++ build_osx_brew.sh
@@ -1,3 +1,5 @@
 /usr/local/share/ocio/setup_ocio.sh
 mkdir -p build && cd build && RM -rf *
 cmake .. -DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX -DCMAKE_PREFIX_PATH=$CONDA_PREFIX/lib -DCMAKE_BUILD_TYPE=Release
+make -j24
+make install
