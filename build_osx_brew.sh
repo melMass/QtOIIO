f2ff9846e2a98fd9e00940683012c9db0bf8e031
melmass <melmassadian@gmail.com>
2019-05-04 23:48:37 +0200

    removes conda dependency

--- build_osx_brew.sh
+++ build_osx_brew.sh
@@ -1,5 +1,5 @@
 /usr/local/share/ocio/setup_ocio.sh
 mkdir -p build && cd build && RM -rf *
-cmake .. -DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX -DCMAKE_PREFIX_PATH=$CONDA_PREFIX/lib -DCMAKE_BUILD_TYPE=Release
+cmake .. -DCMAKE_INSTALL_PREFIX=../install -DCMAKE_PREFIX_PATH=../install/lib -DCMAKE_BUILD_TYPE=Release
 make -j24
 make install
